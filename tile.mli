7fdd529d1d603693f5717236d5aa46c2fb4cf2a4
Max Jiang <qj46@cornell.edu>
2018-04-26 16:50:18 -0400

    Tests done for tile

--- tile.mli
+++ tile.mli
@@ -34,15 +34,29 @@ val num_of_soldier_increase : t -> int
  * [defender_bonus t] reports the defender bonus while defending on the given
  * tile [t]. The min value is 1.0.
  *
+ * Rules of Defender Bonus:
+ * 1. On [Empty] tiles the defender_bonus is the min value 1.0.
+ * 2. On [Fort] and [City i] tiles the defender_bonus is the constant
+ * [GameConstants.fort_city_bonus_factor].
+ * 3. On [Mountain] tiles no soldiers can be on the tile, so the function is
+ * undefined.
+ *
  * Requires: [t] is not [Mountain].
  * @return the bonus for the defender, which tells how much damage on the
  * defender can be reduced with given tile [t].
+ * @raise BadTileInput if [t = Mountain].
 *)
 val defender_bonus : t -> float
 
 (**
  * [upgrade_tile t] upgrades a tile [t] to its new level.
  * [t] cannot be [Mountain].
+ *
+ * Rules of Upgrade Tile:
+ * 1. The upgrade of [Empty] tile is a [Fort] tile.
+ * 2. The upgrade of [Fort] tile is a [City 1] tile.
+ * 3. The upgrade of [City i] tile is a [City (i+1)] tile for integer [i >= 1].
+ * 4. [Mountain] tiles can't be upgraded, so the function is undefined.
  *
  * Requires: [t] is not [Mountain].
  * @return an upgraded tile.
--- tileTests.ml
+++ tileTests.ml
@@ -2,32 +2,53 @@ open OUnit
 open Tile
 open GameConstants
 
-let num_soldier_exception_test (f: Tile.t -> int) (input: Tile.t): unit =
+(* [bad_tile_exception_test] takes in a function [f] and [input] the input
+ * and runs [f input] to check if it has a [BadTileInput] exception. *)
+let bad_tile_exception_test f (input: Tile.t): unit =
   let run _ = f input in
   assert_raises Tile.BadTileInput run
 
+(* [num_soldier_city_test] takes in a integer [k] denoting city level and
+ * returns the right number of soldiers the city will increase. *)
 let num_soldier_city_test (k: int) =
   let m = (float_of_int k) *. GameConstants.increase_soldier_factor in
   int_of_float m
 
 let tests = [
+  (* TODO: Do we need to guard against invalid city levels, like
+     [City 0], [City (-50)]? *)
+
   (* Tile Tests 0-5: Test [num_of_soldier_increase] function. *)
-  "tile_test_soldier_increase-1" >::
+  "tile_test_soldier_increase_1" >::
   (fun _ -> (assert_equal 0 (num_of_soldier_increase Empty)));
-  "tile_test_soldier_increase-2" >::
+  "tile_test_soldier_increase_2" >::
   (fun _ -> (assert_equal 0 (num_of_soldier_increase Fort)));
-  "tile_test_soldier_increase-3" >::
-  (fun _ -> (assert_equal (num_soldier_city_test (-50))
-               (num_of_soldier_increase (City (-50)))));
-  "tile_test_soldier_increase-4" >::
-  (fun _ -> (assert_equal (num_soldier_city_test 0)
-               (num_of_soldier_increase (City 0))));
-  "tile_test_soldier_increase-5" >::
+  "tile_test_soldier_increase_3" >::
+  (fun _ -> (assert_equal (num_soldier_city_test 1)
+               (num_of_soldier_increase (City 1))));
+  "tile_test_soldier_increase_4" >::
   (fun _ -> (assert_equal (num_soldier_city_test 3110)
                (num_of_soldier_increase (City 3110))));
-  "tile_test_soldier_increase-6" >::
-  (fun _ -> num_soldier_exception_test num_of_soldier_increase Mountain);
+  "tile_test_soldier_increase_5" >::
+  (fun _ -> bad_tile_exception_test num_of_soldier_increase Mountain);
 
-  
+  (* Tile tests 6-9: Test [defender_bonus] function. *)
+  "tile_test_defender_bonus_1" >::
+  (fun _ -> (assert_equal 1. (defender_bonus Empty)));
+  "tile_test_defender_bonus_2" >::
+  (fun _ -> (assert_equal fort_city_bonus_factor (defender_bonus Fort)));
+  "tile_test_defender_bonus_3" >::
+  (fun _ -> (assert_equal fort_city_bonus_factor (defender_bonus (City 3110))));
+  "tile_test_defender_bonus_4" >::
+  (fun _ -> (bad_tile_exception_test defender_bonus Mountain));
 
+  (* Tile tests 10-13: Test [upgrade_tile] function. *)
+  "tile_test_defender_bonus_1" >::
+  (fun _ -> (assert_equal Fort (upgrade_tile Empty)));
+  "tile_test_defender_bonus_1" >::
+  (fun _ -> (assert_equal (City 1) (upgrade_tile Fort)));
+  "tile_test_defender_bonus_1" >::
+  (fun _ -> (assert_equal (City 42) (upgrade_tile (City 41))));
+  "tile_test_defender_bonus_1" >::
+  (fun _ -> (bad_tile_exception_test upgrade_tile Mountain));
 ]
