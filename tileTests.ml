37a0e6a5c0b347f416ec6332ba5c908f05773fb4
Max Jiang <qj46@cornell.edu>
2018-04-25 22:49:06 -0400

    partial tests for tile

--- tileTests.ml
+++ tileTests.ml
@@ -1,3 +1,12 @@
 open OUnit
+open Tile
 
-let tests = []
+type t = Empty | Mountain | Fort | City of int
+
+let tests = [
+  "tile_test_works_1" >:: (fun _ -> ());
+  "tile_test_works_2" >:: (fun _ -> (assert_equal Empty Empty));
+
+  "tile_test_num_increase" >::
+  (fun _ -> (assert_equal 0 (num_of_soldier_increase Empty)));
+]
