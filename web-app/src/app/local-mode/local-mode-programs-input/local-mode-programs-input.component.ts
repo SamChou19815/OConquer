1a0894d41ce41674c815c8511537279b1d402d0f
Sam <sam@developersam.com>
2018-04-25 23:41:07 -0400

    Finish local program input

--- web-app/src/app/local-mode/local-mode-programs-input/local-mode-programs-input.component.ts
+++ web-app/src/app/local-mode/local-mode-programs-input/local-mode-programs-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-local-mode-programs-input',
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LocalModeProgramsInputComponent implements OnInit {
 
-  constructor() { }
+  blackProgram: string;
+  whiteProgram: string;
+  @Output() submitted = new EventEmitter<[string, string]>();
+
+  constructor() {
+  }
 
   ngOnInit() {
+  }
+
+  get submitDisabled() {
+    if (this.blackProgram === null || this.blackProgram === undefined
+      || this.whiteProgram === null || this.whiteProgram === undefined) {
+      return true;
+    } else if (this.blackProgram.includes('System.in')
+      || this.blackProgram.includes('System.out')
+      || this.whiteProgram.includes('System.in')
+      || this.whiteProgram.includes('System.out')) {
+      return true;
+    }
+    return false;
+  }
+
+  submit(): void {
+    if (this.submitDisabled) {
+      return;
+    }
+    this.submitted.emit([this.blackProgram, this.whiteProgram]);
   }
 
 }
