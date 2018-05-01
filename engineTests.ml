d3feac8b1a50a35ddd531abf73eb1e2f5f349ee3
Sam <sam@developersam.com>
2018-05-01 17:21:56 -0400

    Added Engine Test

--- engineTests.ml
+++ engineTests.ml
@@ -1,3 +1,14 @@
 open OUnit
+open Definitions
 
-let tests = []
+let tests = [
+  "engine_start_with_in_progress" >:: (fun () ->
+      let open ProgramLists in
+      match Command.from_string black_program_initial white_program_initial with
+      | None -> failwith "Bad!"
+      | Some (b, w) ->
+        let (s, _) = Engine.init b w in
+        assert_equal InProgress (Engine.get_game_status s);
+        Command.stop_program (b, w)
+    )
+]
